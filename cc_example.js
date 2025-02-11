08cd488226aa34b2a1c7936328f65028ce11649b
Eugene Rodriguez <er11@usf.edu>
2025-02-11 14:19:17 -0500

    Task 1 - Product Inventory Managment

---
+++ cc_example.js
@@ -0,0 +1,11 @@
+// Task 1 working with Arrays
+
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Phone", price: 800, category: "Electronics"}
+]
+function getProductsByCategory(products, category) {
+    return products.filter(product => product.category === category)
+}
+
+console.log(getProductsByCategory(products, "Electronics"));
